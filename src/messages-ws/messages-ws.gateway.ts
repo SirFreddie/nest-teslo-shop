3e44617d8cc2ecfaec29cf8ba557cc611918b4e6
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2024-07-07 23:09:24 -0300

    websockets

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,71 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Server;
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.sub);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.unregisterClient(client.id);
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    // Emit the message back to the client
+    // client.emit('message-from-server', {
+    //   fullName: 'John Doe',
+    //   message: payload.message || 'Message received',
+    // });
+
+    // Broadcast the message to all connected clients except the sender
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'John Doe',
+    //   message: payload.message || 'Message received',
+    // });
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'Message received',
+    });
+  }
+}
